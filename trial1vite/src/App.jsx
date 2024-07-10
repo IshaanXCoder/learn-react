0edfd5d53ddf341a2fe2d60d5cceeb2915f59daa
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-07-10 22:40:53 +0530

    learning react

---
+++ trial1vite/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Ishaan from './ishaan.jsx'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+
+    <>
+         <h1>cite react app</h1>
+
+    </>
+
+
+  )
+}
+
+export default App
---
+++ trial1vite/src/ishaan.jsx
@@ -0,0 +1,9 @@
+function Ishaan(){
+    return(
+    <>
+        <h1>Hey ishaan!</h1>
+        <h1>Hey ishaan!</h1>
+        <h1>Hey ishaan!</h1>
+    </>
+    )
+}
