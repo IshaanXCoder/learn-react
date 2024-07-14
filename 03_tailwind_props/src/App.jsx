caec5a9932e547bfd8e56a7d930d485f6801afa4
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-07-14 16:06:56 +0530

    cinnut

--- 03_tailwind_props/src/App.jsx
+++ 03_tailwind_props/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import CardThree from './components/cards'
 
 function App() {
 
   return (
     <>
       <h1 className='text-3xl bg-green-500 p-3 rounded-md' >Hey this is tail trial</h1>
+
+      <CardThree />
+
+
     </>
   )
 }
