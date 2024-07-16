4e5e54c83fe27586e96809847062cf9496718bc6
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-07-16 13:03:58 +0530

    push

--- 03_tailwind_props/src/App.jsx
+++ 03_tailwind_props/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-import './App.css'
-import CardThree from './components/cards'
+import { useState } from "react";
+import "./App.css";
+import Cards from './components/Cards';
 
 function App() {
-
   return (
     <>
-      <h1 className='text-3xl bg-green-500 p-3 rounded-md' >Hey this is tail trial</h1>
+      <h1 className="text-3xl bg-green-500 p-3 h-[200px] w-[300px] rounded-md mb-4">
+        Hey this is tail trial
+      </h1>
 
-      <CardThree />
+      <Cards username="Ihsaan Kesarwani" />
+      <Cards username="Chai aurcode" />
 
-
+     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 03_tailwind_props/src/components/cards.jsx
+++ 03_tailwind_props/src/components/cards.jsx
@@ -1,39 +1,29 @@
-import React from 'react'
-import { ArrowUpRight } from 'lucide-react'
+import React from 'react';
 
-export function CardThree() {
+function Cards({username}) {
+// function Cards(props.username) {
   return (
-    <div className="w-[300px] rounded-md border">
-      <img
-        src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-        alt="Laptop"
-        className="h-[200px] w-full rounded-t-md object-cover"
-      />
-      <div className="p-4">
-        <h1 className="inline-flex items-center text-lg font-semibold">
-          About Macbook &nbsp; <ArrowUpRight className="h-4 w-4" />
-        </h1>
-        <p className="mt-3 text-sm text-gray-600">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?
-        </p>
-        <div className="mt-4">
-          <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-            #Macbook
-          </span>
-          <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-            #Apple
-          </span>
-          <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-            #Laptop
-          </span>
+    <div>
+      <div className="relative h-[400px] w-[250px] rounded-md">
+        <img
+          src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+          alt="AirMax Pro"
+          className="z-0 h-full w-full rounded-md object-cover"
+        />
+        <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+        <div className="absolute bottom-4 left-4 text-left">
+          <h1 className="text-lg font-semibold text-white">{username}</h1>
+          <p className="mt-2 text-sm text-gray-300">
+            ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+            debitis?
+          </p>
+          <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+            View Profile →
+          </button>
         </div>
-        <button
-          type="button"
-          className="mt-4 w-full rounded-sm bg-black px-2 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-        >
-          Read
-        </button>
       </div>
     </div>
-  )
+  );
 }
+
+export default Cards;
