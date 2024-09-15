9fe81185e9d7bd10726bf29261eebff34d6f9c74
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2024-09-15 15:41:35 +0530

    push

---
+++ theme_togler/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Cards from "./components/Cards"
+import ThemeBtn from "./components/ThemeBtn"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+
+      <ThemeBtn />
+      <Cards />
+    </>
+  )
+}
+
+export default App
